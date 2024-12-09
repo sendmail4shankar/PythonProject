dfca27439c6c93636b53bc817fe3fa2c30703572
Devanand Jalla <devanand.jalla@engagely.ai>
2024-12-09 16:00:05 +0530

    initial commit

--- Nov/ex_09112024_Literals_Variables/Lab028_Escape_char.py
+++ Nov/ex_09112024_Literals_Variables/Lab028_Escape_char.py
@@ -1,2 +1,16 @@
 # Escape Sequence
 
+# ''  or ""
+c = 'c'
+
+c1 = "C"
+
+# Where this r concept will be used?
+
+dir = r'C:\shankar\n.txt'
+dir = r"C:\shankar\n.txt"
+print(dir)
+
+
+print("shankar'u\t\ta")
+print(r"shankar'u\t\ta")
